8f7caf1b17f2c828bb7ec7f4a3f34ba5d3d5de4a
PedroGaN <71845931+PedroGaN@users.noreply.github.com>
2020-12-20 15:51:55 +0100

    New/Update/List Ninja functions

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Ninja;
+
 class NinjaController extends Controller
 {
     //
@@ -28,15 +30,17 @@ class NinjaController extends Controller
 			$ninja->skill_inform = $data->skill_inform;
 
 			//Valores opcionales
-			$ninja->rank = (isset($data->rank) ? $data->rank : 'Genin');
-            $ninja->status = (isset($data->status) ? $data->status : 'Active');
+            if(isset($data->rank))
+                $ninja->rank = $data->rank;
+            if(isset($data->status))
+                $ninja->status = $data->status;
 
             //Guardar el ninja
             try{
 
                 $ninja->save();
 
-                $response = "Ninja with name:" + $data->name + " saved successfully";
+                $response = "Ninja with name:" + $ninja->name + " saved successfully";
             }catch(\Exception $e){
                 $response = $e->getMessage();
             }
@@ -45,8 +49,77 @@ class NinjaController extends Controller
 			$response = "Incorrect Data";
 		}
 		
+		return response($response);
+    }
+    
+    public function editNinja(Request $request,$id){
 
+		$response = "";
+
+		//Buscar si existe la nave
+		$ninja = Ninja::find($id);
+
+		if($ninja){
+
+			//Procesar los datos recibidos
+			$data = $request->getContent();
+
+			//Verificar que hay datos
+			$data = json_decode($datos);
+
+			if($data){
+
+				//TODO: validar los datos introducidos
+
+				if(isset($data->name))
+					$ninja->name = $data->name;
+				if(isset($data->skill_inform))
+					$ninja->skill_inform = $data->skill_inform;
+                if(isset($data->rank))
+                    $ninja->rank = $data->rank;
+                if(isset($data->status))
+                    $ninja->status = $data->status;
+
+				//Guardar el ninja
+				try{
+
+					$ninja->save();
+
+					$response = "Ninja with name:" + $ninja->name + " update successfully";
+				}catch(\Exception $e){
+					$response = $e->getMessage();
+				}
+			}else{
+				$response = "Incorrect Data";
+			}
+		}else{
+			$response = "Ninja Not Found";
+		}
 
 		return response($response);
 	}
+
+	public function listNinja(){
+
+		$ninjas = Ninja::all();
+
+		$result = [];
+
+		foreach ($ninjas as $ninja) {
+			
+			$result[] = [
+
+				"name" => $ninja->name,
+				"register_date" => $ninja->created_at,
+                "rank" => $ninja->rank,
+                "status" => $ninja->status
+
+			];
+
+		}
+
+		return response()->json($resultado);
+
+	}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\NinjaController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\MissionController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +20,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('ninjas')->group(function () {
+	Route::post('/new',[NinjaController::class,"newNinja"]);
+	Route::post('/edit/{id}',[NinjaController::class,"editNinja"]);
+	Route::post('/changeStatus/{id}{newStatus}',[NinjaController::class,"statusChangeNinja"]);
+	//Route::get('/check/{id}',[NinjaController::class,"checkPilot"]);
 });
